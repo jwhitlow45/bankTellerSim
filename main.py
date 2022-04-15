2fb8f65a895eb824db9d78f3664937e0a00bf642
jwhitlow45 <jwhitlow45@gmail.com>
2022-04-14 18:12:28 -0700

    feat:add method to generate customer arrival times and work units

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ from scipy.stats import truncnorm
 from queue import PriorityQueue
 
 def main():
-    print('helloworld')
+    for i in generate_customers(160, 8).queue:
+        print(i)
 
 def get_truncated_norm(mean: float, stddev: float, low: float, high: float):
     return truncnorm(
@@ -13,7 +14,19 @@ def get_truncated_norm(mean: float, stddev: float, low: float, high: float):
         scale = stddev
     )
 
-
+def generate_customers(numCustomers: int, maxArrivalTime: float) -> PriorityQueue:
+    customerQueue = PriorityQueue()
+    
+    TruncatedNorm = get_truncated_norm(0.5, 5, 5, 15)
+    # genearte work units
+    workUnits = TruncatedNorm.rvs(numCustomers)
+    
+    for i in range(numCustomers):
+        # randomly generate work units and arrival time
+        customerArirvalTime = np.random.uniform(0, maxArrivalTime)
+        customerQueue.put((customerArirvalTime, workUnits[i]))
+        
+    return customerQueue
     
 if __name__ == '__main__':
     main()
