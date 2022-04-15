9be082b0a3fba88d82a360c65e40a468587f3a9a
jwhitlow45 <jwhitlow45@gmail.com>
2022-04-14 18:04:38 -0700

    feat:add method to create generator of normal distribution

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
+import numpy as np
+from scipy.stats import truncnorm
+from queue import PriorityQueue
+
 def main():
     print('helloworld')
+
+def get_truncated_norm(mean: float, stddev: float, low: float, high: float):
+    return truncnorm(
+        (low - mean) / stddev,
+        (high - mean) / stddev,
+        loc = mean,
+        scale = stddev
+    )
+
+
     
 if __name__ == '__main__':
     main()
